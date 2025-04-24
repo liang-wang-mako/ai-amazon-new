5dad633734f1e41a6734ebf40f01f85bc26a9300
Liang Wang <lwang@micro-wang.com>
2025-04-25 10:31:10 +1200

    Fix export

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,8 +1,10 @@
-import { generateComponents } from '@uploadthing/react';
-import { generateReactHelpers } from '@uploadthing/react/hooks';
+//import { generateComponents } from '@uploadthing/react';
+import { UploadButton, UploadDropzone, Uploader, generateReactHelpers } from '@uploadthing/react';
+
 
 import type { OurFileRouter } from '@/app/api/uploadthing/core';
 
-export const { UploadButton, UploadDropzone, Uploader } = generateComponents<OurFileRouter>();
+//export const { UploadButton, UploadDropzone, Uploader } = generateComponents<OurFileRouter>();
+export { UploadButton, UploadDropzone, Uploader };
 
 export const { useUploadThing, uploadFiles } = generateReactHelpers<OurFileRouter>();
